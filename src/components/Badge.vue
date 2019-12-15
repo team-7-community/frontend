7b4be87e1cd1ce4c9bc13def0a0525b9e6bb467a
Wesley Matos <wesley.matos@redfox.tech>
2019-12-15 13:32:44 -0300

    show companies

---
+++ src/components/Badge.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <badge-bronze
+      v-if="value < 10"
+    />
+    <badge-silver
+      v-bind="$props"
+      v-else-if="value < 40"
+    />
+    <badge-gold
+      v-bind="$props"
+      v-else
+    />
+  </div>
+</template>
+
+<script>
+import BadgeGold from './BadgeGold.vue';
+import BadgeSilver from './BadgeSilver.vue';
+import BadgeBronze from './BadgeBronze.vue';
+
+export default {
+  props: {
+    type: String,
+    value: Number,
+  },
+  components: {
+    BadgeGold,
+    BadgeSilver,
+    BadgeBronze,
+  },
+};
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,122 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="row flex-center">
+    <div class="col-xs-12 col-md-8 row q-gutter-md justify-center q-px-md">
+      <q-card
+        class="col-xs-12 col-sm-3"
+        v-for="(company, index) in companies"
+        :key="index"
+      >
+        <q-card-section>
+          <div> {{ company.name }} {{ index + 1 }} </div>
+          <div class="flex">
+            <div
+              v-for="(badge, index) in company.badges"
+              :key="index"
+            >
+              <badge v-bind="badge"/>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
 <script>
+import Badge from '../components/Badge.vue';
+
 export default {
-  name: 'PageIndex',
+  components: {
+    Badge,
+  },
+  data: () => ({
+    companies: [
+      {
+        name: 'Empresa',
+        badges: [
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Orientação Sexual',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Gênero',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Racial',
+          },
+        ],
+      },
+      {
+        name: 'Empresa',
+        badges: [
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Orientação Sexual',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Gênero',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Racial',
+          },
+        ],
+      },
+      {
+        name: 'Empresa',
+        badges: [
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Orientação Sexual',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Gênero',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Racial',
+          },
+        ],
+      },
+      {
+        name: 'Empresa',
+        badges: [
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Orientação Sexual',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Gênero',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Racial',
+          },
+        ],
+      },
+      {
+        name: 'Empresa',
+        badges: [
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Orientação Sexual',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Gênero',
+          },
+          {
+            value: Math.floor(Math.random() * 100),
+            type: 'Racial',
+          },
+        ],
+      },
+    ],
+  }),
 };
 </script>
